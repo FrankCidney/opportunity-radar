6ed666bccba6525df2cb33379855c543df6f1e90
frawuor <franciscidneyawuor@gmail.com>
2026-02-21 15:37:11 +0300

    chore: add migrations for creating and dropping the jobs table

--- migrations/20260220142958_init.down.sql
+++ migrations/20260220142958_init.down.sql
@@ -1,1 +1,2 @@
 DROP TABLE companies;
+DROP TABLE jobs;
--- migrations/20260220142958_init.up.sql
+++ migrations/20260220142958_init.up.sql
@@ -6,3 +6,23 @@ CREATE TABLE companies (
     created_at TIMESTAMP WITH TIMEZONE NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP WITH TIMEZONE NOT NULL DEFAULT NOW()
 );
+
+CREATE TABLE jobs (
+    id BIGSERIAL PRIMARY KEY,
+    company_id BIGINT NOT NULL REFERENCES companies(id) ON DELETE CASCADE,
+    title TEXT NOT NULL,
+    description TEXT,
+    location TEXT,
+    url TEXT NOT NULL,
+    source TEXT NOT NULL,
+    posted_at TIMESTAMP WITH TIMEZONE,
+    application_deadline TIMESTAMP WITH TIMEZONE,
+    score DOUBLE PRECISION NOT NULL DEFAULT 0,
+    status TEXT NOT NULL DEFAULT 'active'
+        CHECK(status IN ('active', 'archived')),
+    created_at TIMESTAMP WITH TIMEZONE NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIMEZONE NOT NULL DEFAULT NOW()
+);
+
+ALTER TABLE jobs
+ADD CONSTRAINT unique_source_url UNIQUE (source, url);
